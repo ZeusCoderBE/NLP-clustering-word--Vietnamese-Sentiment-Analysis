13985a97db9a83b530726033d878a839ffe6f9a4
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2024-05-07 11:16:03 +0700

    Embedding Model Into web

---
+++ SQL/DBPhone.sql
@@ -0,0 +1,22 @@
+﻿Create DataBase SalesPhone
+go
+use SalesPhone
+go
+CREATE TABLE users (
+    id INT identity PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    full_name nVARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE comments (
+    id INT identity PRIMARY KEY,
+    user_id INT,
+    comment NVARCHAR(MAX),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+INSERT INTO users (username, full_name, password) VALUES
+('user1', N'Nguyễn Văn A', '123'),
+('user2', N'Trần Thị B', '123'),
+('user3', N'Lê Văn C', '123'),
+('user4', N'Phạm Thị D', '123');
